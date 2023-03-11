40d74ea1b79cf98a0c460f6027cf3e5fbfe8d7bb
fjsho <81564274+fjsho@users.noreply.github.com>
2023-03-11 11:59:40 +0900

    feature: get path param

--- src/main/kotlin/com/example/demo/GreeterController.kt
+++ src/main/kotlin/com/example/demo/GreeterController.kt
@@ -1,9 +1,11 @@
 package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping("greeter")
@@ -13,5 +15,12 @@ class GreeterController {
         @RequestParam("name") name: String
     ): HelloResponse {
         return HelloResponse("Hello ${name}!!")
+    }
+
+    @GetMapping("/hello/{name}")
+    fun helloPathValue(
+        @PathVariable("name") name: String
+    ): HelloResponse {
+        return HelloResponse("Hello ${name}!! (path param)")
     }
 }
