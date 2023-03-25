4afd84555dc9d2e783fe03d3a593949893194536
fjsho <81564274+fjsho@users.noreply.github.com>
2023-03-25 09:39:27 +0900

    feature: post request body

--- src/main/kotlin/com/example/demo/GreeterController.kt
+++ src/main/kotlin/com/example/demo/GreeterController.kt
@@ -2,6 +2,8 @@ package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -22,5 +24,12 @@ class GreeterController {
         @PathVariable("name") name: String
     ): HelloResponse {
         return HelloResponse("Hello ${name}!! (path param)")
+    }
+
+    @PostMapping("/hello")
+    fun helloByPost(
+        @RequestBody request: HelloRequest
+    ): HelloResponse {
+        return HelloResponse("Hello ${request.name}!!")
     }
 }
